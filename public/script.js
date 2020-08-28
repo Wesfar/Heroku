fcfbe5afc44d45ddc4f874426839764f1ee20248
JNG <jose.goncalves.pt@gmail.com>
2020-08-28 23:03:59 +0100

    Instrucoes

--- public/script.js
+++ public/script.js
@@ -9,7 +9,8 @@ window.onload = function () { // igual a dizer: window.addEventListener("load", 
 
 function initialize() {
   setupEventListeners();
-  canvases.push(new Canvas ("canvas"));
+  canvases.push(new Canvas ("Canvas_0"));
+  canvases.push(new Canvas ("Canvas_1"));
   newFrame();
 };
 
@@ -18,25 +19,34 @@ function initialize() {
   let socket = io.connect(window.location.origin);
 
 
-function ownDrawing(){
+function displayInstructions(){
   canvases[0].context.beginPath();
-  canvases[0].context.fillStyle = "rgba(200,200,200,1)";
-  canvases[0].context.strokeStyle = "rgba(0,0,0,1)";
-  canvases[0].context.arc(touchStartX || mouseX, touchStartY || mouseY, 5, 0, 2*Math.PI);
-  canvases[0].context.fill();
-  canvases[0].context.stroke();
+  canvases[0].context.fillStyle = "darkblue";
+
+  canvases[0].context.fillText("Carregar em qualquer parte do ecrã", 10, 20);
+  canvases[0].context.fillText("A imagem da bola aparece em todos", 10, 30);
+  canvases[0].context.fillText("os ecrãs dos users que estiverem no site!", 10, 40);
+  };
+
+function ownDrawing(){
+  canvases[1].context.beginPath();
+  canvases[1].context.fillStyle = "rgba(200,200,200,1)";
+  canvases[1].context.strokeStyle = "rgba(0,0,0,1)";
+  canvases[1].context.arc(touchStartX || mouseX, touchStartY || mouseY, 5, 0, 2*Math.PI);
+  canvases[1].context.fill();
+  canvases[1].context.stroke();
   };
 
 
 // Receber mensagem "mouse" do Server e executar função externalDrawing(data)
 socket.on("mouseBroadcast", externalDrawing);
 function externalDrawing (data){
-    canvases[0].context.beginPath();
-    canvases[0].context.fillStyle = "rgba(0,0,200,1)";
-    canvases[0].context.strokeStyle = "rgba(0,0,0,1)";
-    canvases[0].context.arc(data.x, data.y, 5, 0, 2*Math.PI);
-    canvases[0].context.fill();
-    canvases[0].context.stroke();
+    canvases[1].context.beginPath();
+    canvases[1].context.fillStyle = "rgba(0,0,200,1)";
+    canvases[1].context.strokeStyle = "rgba(0,0,0,1)";
+    canvases[1].context.arc(data.x, data.y, 5, 0, 2*Math.PI);
+    canvases[1].context.fill();
+    canvases[1].context.stroke();
     };
   
 
@@ -51,8 +61,8 @@ function sendDataToServer(){
 }
 
 function newFrame() {
-  //  canvases[0].clear();
-
+  canvases[0].clear();
+  displayInstructions ();
   // Code Here
   if(mouseClicked || touchLive){
     ownDrawing();
